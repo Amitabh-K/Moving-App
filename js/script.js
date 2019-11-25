4f98e3a1d0b7cc00083549d3c0e2e4cd9fd055b6
Amitabh Kumar <amitabh.kumar2@gmail.com>
2019-11-25 08:26:03 -1000

    bg image of sdt view gogle maps fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
 function loadData() {
 
+    /*
+    The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
+    */
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -11,19 +14,40 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
     // load streetview
-    var streetStr = $("#street").val();
-    var cityStr = ("#city").val();
-    var address = streetStr + ', ' +cityStr;
-
-    $greeting.text("So, you would loke to live at " + address + "?");
-
-    var streetviewUrl = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + address + "";
-    $body.append('img class="bgimg" src="' + streetviewUrl + '">');
+    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?location=' + address + '' + '&size=600x300&key=AIzaSyCrDB76TKDys1WqrVlgYYOqZRU5_77PGdg';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
 
+    // load nytimes
+    // obviously, replace all the "X"s with your own API key
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=4AULT44ePbU81fhpCoekcBokpzoCiLIm';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
 
 
+    // load wikipedia data
 
     // YOUR CODE GOES HERE!
 
@@ -31,5 +55,3 @@ function loadData() {
 };
 
 $('#form-container').submit(loadData);
-
-// loadData();
